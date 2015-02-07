b5e5534144473ebd7e1282810bbbe76205eef68b
jeans <entelechy39@gmail.com>
2015-02-08 01:03:15 +0900

    Modify velocity, Add avatar reset, Add Timer.

--- main.py
+++ main.py
@@ -44,46 +44,112 @@ viz.eyeheight(0)
 
 avatar = viz.add('soccerball.ive')
 avatar.setPosition(1,0.5,10)
-avatar.collideMesh()
+avatar.collideSphere()
 avatar.enable(viz.COLLIDE_NOTIFY)
-
-def avatar_move():
-	avatar.setVelocity([0,0,-3], viz.ABS_GLOBAL)
-	
-
-
 
 import random
 
 car = viz.add('soccerball.ive')
-car.collideMesh()
+car.collideSphere()
 
 car_loc = random.randint(0,4)
 
-if car_loc == 1 :
+if car_loc%2 == 1 :
 	car.setPosition(6,0.5,5)
 else :
 	car.setPosition(-4,.5,5)
 
-def car_move():
+
+def avatar_move():
+	avatar.setVelocity([0,0,-3], viz.ABS_GLOBAL)
+	
 	if car_loc%2 == 1:
-		#car.setPosition([-.01, 0, 0], viz.ABS_LOCAL)
-		#car.setEuler([-2, 0, 0], viz.REL_PARENT)
-		car.setVelocity([-1,0,0], viz.ABS_GLOBAL)
+		car.setVelocity([-3,0,0], viz.ABS_GLOBAL)
 	else :
-		#car.setPosition([.01, 0, 0], viz.ABS_LOCAL)
-		#car.setEuler([2, 0, 0], viz.REL_PARENT)
-		car.setVelocity([1,0,0], viz.ABS_GLOBAL)
+		car.setVelocity([3,0,0], viz.ABS_GLOBAL)
 
 
+##########################Timer########################
+
+
+import viztask
+
+
+def Reset():
+	global car_loc
+	
+	avatar.reset()
+	avatar.setPosition(1,0.5,10)
+	
+	car_loc = random.randint(0,4)
+	car.reset()
+	if car_loc%2 == 1 :
+		car.setPosition(6,.5,5)
+	else :
+		car.setPosition(-4,.5,5)
 
 
 def onCollideBegin(e):
 	if e.obj2 == car:
 		print 'collide!'
+		
 
-viz.callback(viz.COLLIDE_BEGIN_EVENT, onCollideBegin)
-viz.phys.enable()
+def TestReactionTime():
+	while True:
+		yield viztask.waitTime(vizmat.GetRandom(1.5,2.5))
+		
+		avatar_move()
+		
+		d = yield viztask.waitDraw()
+		
+		#Save start time
+		startTime = d.time
+
+		#Wait for mouse reaction
+		
+		#d = yield viztask.waitEvent(viz.COLLIDE_BEGIN_EVENT)
+		d = yield viztask.waitMouseDown(viz.MOUSEBUTTON_LEFT)
+		
+		
+		#Calculate reaction time
+		reactionTime = d.time - startTime
+
+        #Print time
+
+		print 'Reaction time:',reactionTime
+		Reset()
+
+
+		
+viz.callback(viz.COLLIDE_BEGIN_EVENT, onCollideBegin)		
+viztask.schedule( TestReactionTime() )
 
 vizact.onkeydown(' ', avatar_move)
-vizact.onkeydown(' ', car_move)
+viz.phys.enable()
+
+
+
+
+
+
+
+#############################################Ex############################################
+import viztask
+
+CUSTOM_EVENT = viz.getEventID("super special event")
+
+def MyTask():
+    
+    while True:
+        
+        d = yield viztask.waitEvent(CUSTOM_EVENT,all=True)
+        
+        print 'Event data:'
+        for e in d.data:
+            print e[0]
+
+viztask.schedule( MyTask() )
+
+vizact.onkeydown(' ',viz.sendEvent,CUSTOM_EVENT,4)
+vizact.onkeydown(' ',viz.sendEvent,CUSTOM_EVENT,5)
+vizact.onkeydown(' ',viz.sendEvent,CUSTOM_EVENT,6)
