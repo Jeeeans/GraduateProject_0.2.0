a4cf0df5e580f6170a8621b7d9dc1a4167bcadb9
jeans <entelechy39@gmail.com>
2015-02-15 23:12:18 +0900

    Refactory

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import viztask
 viz.go()
 
 #####################BackGround###################
-
 ground = viz.add('art/sphere_ground3.ive')
 ground.collidePlane(0,1,0,0)
 env = viz.add(viz.ENVIRONMENT_MAP,'sky.jpg')
@@ -32,20 +31,17 @@ def onMouseMove(e):
 viz.callback(viz.MOUSE_MOVE_EVENT, onMouseMove)
 viz.mouse(viz.OFF)
 #####################Avatar#####################
-
 avatar = viz.add('soccerball.ive')
 avatar.setPosition(0,0,11)
 avatar.collideSphere()
 avatar.enable(viz.COLLIDE_NOTIFY)
-
 
 #####################Car########################
 car = viz.add('soccerball.ive')
 car.collideSphere()
 
 carLoc = random.randint(0,2)
-car.setPosition(10,.5,5)
-
+car.setPosition(10,100,5)
 
 ############################Timer#########################
 time = 0
@@ -54,6 +50,7 @@ def Timer():
 	time = time + 0.01
 
 vizact.ontimer(0.01, Timer)
+
 ##########################Functions########################
 level = 1
 stage = 6
@@ -80,7 +77,7 @@ def Reset():
 	
 	carLoc = random.randint(0,2)
 	car.reset()
-	car.setPosition(10,0,5)
+	car.setPosition(10,100,5)
 
 def GetCarPosition():
 	global carPosition
@@ -93,34 +90,20 @@ def GetCarPosition():
 	if carPosition[0] != 10 :
 		if carLoc%2 == 1 :
 			if carPosition[0] < stage/3 :
-				visualFlag = True
-			elif carPosition[0] < stage*2/3 :
 				auditoryFlag = True
+			elif carPosition[0] < stage*2/3 :
+				visualFlag = True
 		else :
 			if -1*carPosition[0] < stage/3 :
-				visualFlag = True
-			elif -1*carPosition[0] < stage*2/3 :
 				auditoryFlag = True
+			elif -1*carPosition[0] < stage*2/3 :
+				visualFlag = True
 	
-def onCollideBegin(e):
-	global stage
-	global level
-	global collideFlag
-	if e.obj2 == car:
-		print 'collide!'
-		collideFlag = True
-		if stage < 6 :
-			stage = stage + 1
-		else :
-			level = level - 1
-
-
 ###############################Training##############################
 def TestReactionTime():
 	global time
 	global level
 	global stage
-	global collideFlag
 	global visualFlag
 	global auditoryFlag
 	
@@ -131,7 +114,8 @@ def TestReactionTime():
 	visualCue = 0
 	auditoryCue = 0
 	
-	
+	waitMouse = viztask.waitMouseDown(viz.MOUSEBUTTON_LEFT)
+	waitCollide = viztask.waitEvent(viz.COLLIDE_BEGIN_EVENT)
 	#Start test from space down
 	yield viztask.waitKeyDown(' ')
 	
@@ -152,28 +136,30 @@ def TestReactionTime():
 		startTime = time
 		
 		#Wait for mouse reaction or collide event
-		d = yield viztask.waitAny([viztask.waitMouseDown(viz.MOUSEBUTTON_LEFT), viztask.waitEvent(viz.COLLIDE_BEGIN_EVENT)])
+		d = yield viztask.waitAny([waitMouse, waitCollide])
 		reactionTime = time - startTime
-		if level < 4 :
-			level = level + 1
-		elif stage > 1 :
-			stage = stage - 1
-		
-		#Calculate reaction time
-		
-
-        #Print time		
-
-		if collideFlag :
-			taskFail += 1
-		else :
+		if d.condition is waitMouse:
+			if level < 4 :
+				level = level + 1
+			elif stage > 1 :
+				stage = stage - 1
+				
 			if carLoc%2 == 1:
 				leftReactionTime = 0
 				rightReactionTime = reactionTime
 			else :
 				leftReactionTime = reactionTime
 				rightReactionTime = 0
-				
+		elif d.condition is waitCollide:
+			if stage < 6 :
+				stage = stage + 1
+			elif level > 1 :
+				level = level - 1
+			taskFail += 1
+		
+		
+		#Calculate reaction time
+		
 		if visualFlag :
 			visualCue += 1
 			visualFlag = False
@@ -187,7 +173,6 @@ def TestReactionTime():
 		print 'Left Reaction Time : ',leftReactionTime
 		print 'Right Reaction Time : ',rightReactionTime
 		
-		collideFlag = False
 		testCount += 1
 		Reset()
 		
@@ -195,12 +180,9 @@ def TestReactionTime():
 	print 'auditorycue : ',auditoryCue
 	print 'FailCount : ',taskFail
 
-
-##############################################################################
-
 viztask.schedule(TestReactionTime())
-vizact.ontimer(0.01, GetCarPosition)
-viz.callback(viz.COLLIDE_BEGIN_EVENT, onCollideBegin)		
+##############################################################################
+vizact.ontimer(0.01, GetCarPosition)	
 
 #don't move out mouse pointer
 viz.mouse.setTrap()
